b904675e1bb8f71701a59ed8c97a6be7f1b99f9a
vipultiwari22 <tiwarivipul155@gmail.com>
2024-02-16 20:49:40 +0530

    deletedFiles

--- BackendWithExpress/OS Module/index.js
+++
@@ -1,9 +0,0 @@
-const os = require("os");
-
-console.log(os.arch());
-console.log(os.platform());
-console.log(os.freemem());
-console.log(os.release());
-console.log(os.tmpdir());
-console.log(os.endianness());
-console.log(os.totalmem());
