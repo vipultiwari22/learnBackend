96a1a4f094e61672f2d40dfc7b8d08af8436fef5
vipultiwari22 <tiwarivipul155@gmail.com>
2024-02-18 06:56:13 +0530

    code update

--- Project/Backend/routes/authRoute.js
+++ Project/Backend/routes/authRoute.js
@@ -4,6 +4,6 @@ const authRouter = express.Router();
 
 authRouter.get("/", Home);
 authRouter.post("/api/auth/singup", signup);
-authRouter.post("/singin", singin);
+authRouter.get("/singin", singin);
 
 module.exports = authRouter;
