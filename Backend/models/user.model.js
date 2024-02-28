94587970279b5e1aaf471c9fe6375397a0c65e1b
vipultiwari22 <tiwarivipul155@gmail.com>
2024-02-28 21:35:48 +0530

    New file folder added

---
+++ Backend/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    requird: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowecase: true,
+    minLength: 10,
+    unique: true,
+  },
+  userType: {
+    type: String,
+    required: true,
+    defulat: "CUSTOMER",
+    enum: ["CUSTOMER", "ADMIN"],
+  },
+});
